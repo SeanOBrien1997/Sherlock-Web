460c1ec05feb9e83219ef7035cfef0e846a1314b
Sean O Brien <sean.obrien.2016@mumail.ie>
2021-04-27 19:42:21 +0100

    Update server to return JSON with count of sites.

--- myserver/index.js
+++ myserver/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const spawn = require('child_process').spawn;
+const fs = require('fs');
+const eol = require('eol');
 
 const app = express();
 const PYTHON_COMMAND = 'python3';
@@ -10,8 +12,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
 	const username = req.query.username;
-	const sherlock = spawn(PYTHON_COMMAND, [SHERLOCK_PATH, username]);
+	if (username === undefined) {
+		res.send('Username undefined');
+		return;
+	}
 
+	const sherlock = spawn(PYTHON_COMMAND, [SHERLOCK_PATH, username]);
 	let outputChunks = [];
 
 	sherlock.on('exit', (exitCode) => {
@@ -23,8 +29,31 @@ app.get('/', (req, res) => {
 	});
 
 	sherlock.stdout.on('end', () => {
+		let response = {
+			count: 0,
+			sites: {},
+		};
 		const stdoutContent = Buffer.concat(outputChunks).toString();
-		res.send(stdoutContent);
+		let lines = eol.split(stdoutContent);
+		lines.shift(); // get rid of checking username line
+		lines.forEach((line) => {
+			line = line.replace('[+]', '');
+			let urlData = line.split(' http');
+			if (!(urlData[1] === undefined)) {
+				const siteName = urlData[0].replace(':', '').trim();
+				const siteURL = 'http'.concat(urlData[1]);
+				response.sites[siteName] = siteURL;
+			}
+		});
+		response.count = Object.keys(response.sites).length;
+		res.send(response);
+		const fileName = `./${username}.txt`;
+		fs.unlink(fileName, (err) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		});
 	});
 });
 
